703811155601fd30bde5085b9cedc7b4e4dd6b29
watanabejin <jin.watanabe@uzabase.com>
2023-02-16 09:31:01 +0900

    Add before senario

--- src/test/kotlin/hello.kt
+++ src/test/kotlin/hello.kt
@@ -1,9 +1,22 @@
+import com.thoughtworks.gauge.AfterScenario
+import com.thoughtworks.gauge.BeforeScenario
 import com.thoughtworks.gauge.Step
+import com.github.tomakehurst.wiremock.client.WireMock
 
 class HelloGitHubActions {
+
+//    @BeforeSuite
+//    @BeforeSpec
+    @BeforeScenario
+    fun prepareWireMock() {
+        WireMock.reset()
+    }
+
+
 
     @Step("HelloWorldを出力する")
     fun hello(){
         println("Hello, GitHub Action")
     }
+
 }
